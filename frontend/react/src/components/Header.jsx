be89b16fff6e2c3fbfa5f1ef8e792c01c7192cb8
pasyg <pgekle@gmail.com>
2025-12-18 10:43:15 +0100

    Restructure layout, fix Sidebar

--- frontend/react/src/components/Header.jsx
+++ frontend/react/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({title}) {
   return (
-    <header className="bg-neutral-800 h-24 flex items-center justify-between px-6">
-      <h1 className="text-3xl text-gray-200">{title}</h1>
+    <header className="bg-neutral-800 h-24 flex items-center justify-between px-6 ml-64">
+      <h1 className="text-2xl text-gray-200">{title}</h1>
 
       <div className="header-right">
         <a href="/user" className="hover:bg-gray-500 p-2 rounded cursor-pointer">User</a>
